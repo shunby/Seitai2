5a96b9de60137b51f3cbf01d9b0340dba95a2cee
shunby <taby@outlook.jp>
2017-01-30 17:28:41 +0900

    1/30 graph

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -9,15 +9,17 @@ import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
 import seitai.Main;
 
 public class Graph {
+	static Random r;
 
 	public static void main(String[] args) {
-
+		r = new Random();
 	    BufferedImage readImage = null;
 	    if (readImage == null){
 	      readImage = new BufferedImage(1000, 1000,
@@ -29,7 +31,6 @@ public class Graph {
 	    drawGraph(off);
 
 	    try {
-	      boolean result =
 	       ImageIO.write(readImage, "png", new File("graph.png"));
 	    } catch (Exception e) {
 	        e.printStackTrace();
@@ -37,22 +38,29 @@ public class Graph {
 	  }
 
 	private static void drawGraph(Graphics2D g) {
-		int ge_num = 300;
-		int fe_num = 20;
+		int ge_num = 30000;
+		int fe_num = 200;
 		int se_num = 0;
 		int time = 0;
 
-		double ge_increase = 0.0125d;
-		double ge_decrease = 0.0025d;
-		double fe_increase = 0.005d;
-		double fe_decrease = 0.0025d;
-		double fe_decrease_2 = 0.00125d;
-		double se_increase = 0.00075d;
-		double se_decrease = 0.00025d;
+		double ge_increase =gen();
+		double ge_decrease = gen();
+		double fe_increase = gen();
+		double fe_decrease = gen();
+		double fe_decrease_2 = gen();
+		double se_increase = gen();
+		double se_decrease = gen();
+		System.out.println("gi="+ge_increase + "\n"
+				 + "gd=" + ge_decrease + "\n"
+				 + "fi=" + fe_increase + "\n"
+				 + "fd0=" + fe_decrease + "\n"
+				 + "fd1=" + fe_decrease_2 + "\n"
+				 + "si=" + se_increase + "\n"
+				 + "sd=" + se_decrease + "\n");
 
 		System.out.println("  ge  |  fe   |   se ");
 
-		for(time = 0; time <= 1000; time++){
+		for(time = 0; time <= 100; time++){
 			double ga = ge_increase * ge_num - ge_decrease * ge_num * fe_num;
 			double fa = fe_increase * ge_num * fe_num - fe_decrease * fe_num - fe_decrease_2 * fe_num * se_num;
 			double sa = se_increase * fe_num * se_num - se_decrease * se_num;
@@ -81,6 +89,10 @@ public class Graph {
 
 
 
+	}
+
+	static double gen(){
+		return r.nextInt(10000000) / 100000000d;
 	}
 
 }
